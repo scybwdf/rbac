c887b342862746b00abd16a400515727d7b3ebf6
root <root@felix.localdomain>
2018-01-17 09:34:50 +0800

    修改：      api/controllers/CachetestController.php

--- api/controllers/CachetestController.php
+++ api/controllers/CachetestController.php
@@ -4,7 +4,8 @@ use yii\web\Controller;
 class CachetestController extends Controller{
     public function actionIndex(){
         $cache=\YII::$app->cache;
-        $cache->add('name',12);
+        $cache->add('name',12,15);
+      //  $cache->delete('name');
         echo $cache->get('name');
     }
 }
